b411f6501468fd6567516b6c4d9af1840c97ef55
Dmitry <dmitry.kremenetsky1@gmail.com>
2023-07-05 09:15:18 +0300

    fix: change form validation functionality to match email and password

--- src/js/confirm.js
+++ src/js/confirm.js
@@ -14,6 +14,8 @@ document.addEventListener("DOMContentLoaded", function () {
 	function checkFormFields() {
 		const inputs = form.querySelectorAll("input");
 		let hasEmptyFields = false;
+		let isValidEmail = true;
+		let isValidPassword = true;
 
 		inputs.forEach(function (input) {
 			if (input.value.trim() === "") {
@@ -22,10 +24,28 @@ document.addEventListener("DOMContentLoaded", function () {
 			} else {
 				input.classList.remove("error");
 			}
+
+			if (input.id === "email" && !validateEmail(input.value)) {
+				input.classList.add("error");
+				isValidEmail = false;
+			}
+
+			if (input.id === "password" && !validatePassword(input.value)) {
+				input.classList.add("error");
+				isValidPassword = false;
+			}
 		});
 
 		if (hasEmptyFields) {
 			console.log("Пожалуйста, заполните все обязательные поля.");
+		} else if (!isValidEmail) {
+			console.log(
+				"Пожалуйста, введите действительный адрес электронной почты."
+			);
+		} else if (!isValidPassword) {
+			console.log(
+				"Пароль должен содержать минимум 8 символов, заглавные и строчные буквы, а также цифры."
+			);
 		} else {
 			mainContent.style.display = "none";
 			registrationSuccess.style.display = "block";
@@ -33,3 +53,13 @@ document.addEventListener("DOMContentLoaded", function () {
 		}
 	}
 });
+
+function validateEmail(email) {
+	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return regex.test(email);
+}
+
+function validatePassword(password) {
+	const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+	return regex.test(password);
+}
